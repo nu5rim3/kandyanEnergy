9fe2fa103367ecd12a2581b707ba31e106ec64ba
nu5rim3 <nusrisamath@gmail.com>
2021-08-26 13:06:50 +0530

    updated feedback section with image

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import image_8 from '../assets/images/8.png'
+import solar_1 from '../assets/images/solar_1.jpg'
+import solar_2 from '../assets/images/solar_2.jpg'
+import solar_3 from '../assets/images/solar_3.jpg'
+import solar_4 from '../assets/images/solar_4.jpg'
 
 
 function Feedback() {
@@ -28,34 +32,34 @@ function Feedback() {
                         <div className="Section__feedback__bottom__topic text-white font-semibold text-1xl pb-5">See our work</div>
                         <div className="grid grid-cols-4">
                             <div className="Section__feedback__bottom__card pr-12">
-                                <img src="https://via.placeholder.com/250x200" alt="kandyan energy" className="Section__feedback__bottom__card__image rounded-xl" />
+                                <img src={solar_1} alt="kandyan energy" className="Section__feedback__bottom__card__image h-52 rounded-xl" />
                                 <div className="flex flex-wrap justify-between pt-3">
                                     <div className="Section__feedback__bottom__card__type text-xs text-white font-semibold">solarX panel</div>
-                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 24kW</div>
+                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 8400kW</div>
                                 </div>
                                 <div className="Section__feedback__bottom__card__location text-1xl font-extrabold text-green-400">Kelaniya</div>
                             </div>
                             <div className="Section__feedback__bottom__card pr-12">
-                                <img src="https://via.placeholder.com/250x200" alt="kandyan energy" className="Section__feedback__bottom__card__image rounded-xl" />
+                                <img src={solar_2} alt="kandyan energy" className="Section__feedback__bottom__card__image h-52 rounded-xl" />
                                 <div className="flex flex-wrap justify-between pt-3">
                                     <div className="Section__feedback__bottom__card__type text-xs text-white font-semibold">solarX panel</div>
-                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 332kW</div>
+                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 32000kW</div>
                                 </div>
                                 <div className="Section__feedback__bottom__card__location text-1xl font-extrabold text-green-400">Mawilmada</div>
                             </div>
                             <div className="Section__feedback__bottom__card pr-12">
-                                <img src="https://via.placeholder.com/250x200" alt="kandyan energy" className="Section__feedback__bottom__card__image rounded-xl" />
+                                <img src={solar_3} alt="kandyan energy" className="Section__feedback__bottom__card__image h-52 rounded-xl" />
                                 <div className="flex flex-wrap justify-between pt-3">
                                     <div className="Section__feedback__bottom__card__type text-xs text-white font-semibold">solarX panel</div>
-                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 120kW</div>
+                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 120000kW</div>
                                 </div>
                                 <div className="Section__feedback__bottom__card__location text-1xl font-extrabold text-green-400">Ambilipitiya</div>
                             </div>
                             <div className="Section__feedback__bottom__card pr-12">
-                                <img src="https://via.placeholder.com/250x200" alt="kandyan energy" className="Section__feedback__bottom__card__image rounded-xl" />
+                                <img src={solar_4} alt="kandyan energy" className="Section__feedback__bottom__card__image h-52 rounded-xl" />
                                 <div className="flex flex-wrap justify-between pt-3">
                                     <div className="Section__feedback__bottom__card__type text-xs text-white font-semibold">solarX panel</div>
-                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 32kW</div>
+                                    <div className="Section__feedback__bottom__card__volt text-sm text-green-400 font-extrabold">⚡ 53000kW</div>
                                 </div>
                                 <div className="Section__feedback__bottom__card__location text-1xl font-extrabold text-green-400">Aladeniya</div>
                             </div>
